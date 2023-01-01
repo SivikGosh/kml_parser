f8dea1cb2c72c46703802bb0752d6ba7aa21eb61
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2023-01-01 18:24:19 +0300

    добавил функцию создания папок

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-# from fastkml import kml
+import os
+
+from fastkml import kml
 from get_pictograms import open_input_file
 from progress.bar import PixelBar
 
@@ -13,7 +15,23 @@ def get_objects_styles(soup):
     return list(styles)
 
 
+def get_folders(base_dir, soup):
+    folders = []
+    bar = PixelBar(max=len(soup.find_all('Folder')))
+    for i in range(len(soup.find_all('Folder'))):
+        folders.append(soup.find_all('Folder')[i].find('name').string)
+        if not os.path.isdir(folders[i]):
+            os.mkdir(f'{base_dir}/{folders[i]}')
+        bar.next()
+    bar.finish()
+    return folders
+
+
+def create_kml_files(folders, styles):
+    pass
+
+
 if __name__ == '__main__':
     base_dir, input_file, soup = open_input_file()
     styles = get_objects_styles(soup)
-    print(styles)
+    folders = get_folders(base_dir, soup)
