ac8d7fcfb24ede52528f5252212eb146ed611eab
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2023-01-04 20:23:41 +0300

    поправил загрузку пиктограмм

--- app/get_pictograms.py
+++ app/get_pictograms.py
@@ -2,7 +2,7 @@ import os
 import re
 
 import requests
-from bs4 import BeautifulSoup, CData
+from bs4 import BeautifulSoup
 from progress.bar import PixelBar
 
 
@@ -15,21 +15,21 @@ def open_input_file():
 
 
 def get_icon_urls(soup):
-    output_url_list = set()
-    href_amount = soup.find_all('href')
-    bar = PixelBar(max=len(href_amount))
-    for href in href_amount:
-        output_url_list.add(href.text)
+    urls = set()
+    all_hrefs = soup.find_all('href')
+    bar = PixelBar(max=len(all_hrefs))
+    for href in all_hrefs:
+        urls.add(href.text)
         bar.next()
     bar.finish()
-    return list(output_url_list)
+    return list(urls)
 
 
 def download_icons(urls):
     bar = PixelBar(max=len(urls))
     for url in urls:
         img = requests.get(url)
-        img_name = (re.search('[0-9-a-z_]+.png', url).group(0))
+        img_name = re.search('[0-9-a-z_]+.png', url).group(0)
         img_option = open(img_name, 'wb')
         img_option.write(img.content)
         img_option.close()
@@ -39,11 +39,9 @@ def download_icons(urls):
 
 if __name__ == '__main__':
     base_dir, input_file, soup = open_input_file()
-    c_data = CData('<title>Hello</title>')
-    print('<title>Hello</title>')
-    # urls = get_icon_urls(soup)
-    # os.chdir(base_dir)
-    # if not os.path.isdir('pictograms'):
-    #     os.mkdir('pictograms')
-    # os.chdir(f'{base_dir}/pictograms')
-    # download_icons(urls)
+    urls = get_icon_urls(soup)
+    os.chdir(base_dir)
+    if not os.path.isdir('pictograms'):
+        os.mkdir('pictograms')
+    os.chdir(f'{base_dir}/pictograms')
+    download_icons(urls)
