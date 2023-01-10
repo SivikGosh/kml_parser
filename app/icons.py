ca6e803739f9116d1cff3a45d75f88f47d2f335e
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2023-01-11 01:32:14 +0300

    делаю словарь

---
+++ app/icons.py
@@ -0,0 +1,214 @@
+icons = {
+    'icon-1001.png': [
+        '#icon-1001',
+        '#icon-1001-labelson',
+        '#icon-1001-labelson-nodesc',
+        '#icon-1001-nodesc'
+    ],
+    'icon-1019.png': [
+        '#icon-1019-labelson',
+        '#icon-1019-labelson-nodesc'
+    ],
+    'icon-1037.png': [
+        '#icon-1037',
+        '#icon-1037-labelson',
+        '#icon-1037-labelson-nodesc',
+        '#icon-1037-nodesc'
+    ],
+    'icon-1047.png': [
+        '#icon-1047',
+        '#icon-1047-labelson',
+        '#icon-1047-labelson-nodesc',
+        '#icon-1047-nodesc'
+    ],
+    'icon-1113.png': [
+        '#icon-1113',
+        '#icon-1113-labelson',
+        '#icon-1113-labelson-nodesc',
+        '#icon-1113-nodesc'
+    ],
+    'icon-1119.png': [
+        '#icon-1119',
+        '#icon-1119-labelson',
+        '#icon-1119-labelson-nodesc'
+    ],
+    'icon-1137.png': [
+        '#icon-1137-labelson',
+        '#icon-1137-nodesc'
+    ],
+    'icon-1121.png': [
+        '#icon-1121',
+        '#icon-1121-labelson',
+        '#icon-1121-labelson-nodesc',
+        '#icon-1121-nodesc'
+    ],
+    'icon-1149.png': [
+        '#icon-1149',
+        '#icon-1149-labelson',
+        '#icon-1149-labelson-nodesc',
+        '#icon-1149-nodesc'
+    ],
+    'icon-1151.png': [
+        '#icon-1151',
+        '#icon-1151-labelson',
+        '#icon-1151-nodesc'
+    ],
+    'icon-1193.png': [
+        '#icon-1193',
+        '#icon-1193-labelson'
+    ],
+    'icon-1199.png': [
+        '#icon-1199',
+        '#icon-1199-labelson',
+        '#icon-1199-labelson-nodesc',
+        '#icon-1199-nodesc'
+    ],
+    'icon-1239.png': [
+        '#icon-1239-labelson'
+    ],
+    'icon-1273.png': [
+        '#icon-1273'
+    ],
+    'icon-1313.png': [
+        '#icon-1313-nodesc'
+    ],
+    'icon-1337.png': [
+        '#icon-1337',
+        '#icon-1337-labelson',
+        '#icon-1337-nodesc'
+    ],
+    'icon-1343.png': [
+        '#icon-1343',
+        '#icon-1343-nodesc'
+    ],
+    'icon-1345.png': [
+        '#icon-1345'
+    ],
+    'icon-1349.png': [
+        '#icon-1349'
+    ],
+    'icon-1351.png': [
+        '#icon-1351',
+        '#icon-1351-labelson'
+    ],
+    'icon-503-009D57.png': [
+        '#icon-503-009D57',
+        '#icon-503-009D57-labelson',
+        '#icon-503-009D57-labelson-nodesc',
+        '#icon-503-009D57-nodesc'
+    ],
+    'icon-503-0BA9CC.png': [
+        '#icon-503-0BA9CC',
+        '#icon-503-0BA9CC-labelson',
+        '#icon-503-0BA9CC-labelson-nodesc',
+        '#icon-503-0BA9CC-nodesc'
+    ],
+    'icon-503-4186F0.png': [
+        '#icon-503-4186F0',
+        '#icon-503-4186F0-labelson',
+        '#icon-503-4186F0-labelson-nodesc',
+        '#icon-503-4186F0-nodesc'
+    ],
+    'icon-503-62AF44.png': [
+        '#icon-503-62AF44',
+        '#icon-503-62AF44-labelson-nodesc'
+    ],
+    'icon-503-795046.png': [
+        '#icon-503-795046-labelson',
+        '#icon-503-795046-labelson-nodesc'
+    ],
+    'icon-503-7C3592.png': [
+        '#icon-503-7C3592',
+        '#icon-503-7C3592-labelson',
+        '#icon-503-7C3592-labelson-nodesc'
+    ],
+    'icon-503-93D7E8.png': [
+        '#icon-503-93D7E8'
+    ],
+    'icon-503-B29189.png': [
+        '#icon-503-B29189-labelson',
+        '#icon-503-B29189-labelson-nodesc'
+    ],
+    'icon-503-CDDC39.png': [
+        '#icon-503-CDDC39-labelson'
+    ],
+    'icon-503-DB4436.png': [
+        '#icon-503-DB4436',
+        '#icon-503-DB4436-labelson',
+        '#icon-503-DB4436-labelson-nodesc',
+        '#icon-503-DB4436-nodesc'
+    ],
+    'icon-503-F4B400.png': [
+        '#icon-503-F4B400-labelson'
+    ],
+    'icon-503-F8971B.png': [
+        '#icon-503-F8971B',
+        '#icon-503-F8971B-labelson',
+        '#icon-503-F8971B-labelson-nodesc'
+    ],
+    'icon-503-FFDD5E.png': [
+        '#icon-503-FFDD5E'
+    ],
+    'icon-959-009D57.png': [
+        '#icon-959-009D57',
+        '#icon-959-009D57-labelson',
+        '#icon-959-009D57-nodesc'
+    ],
+    'icon-959-62AF44.png': [
+        '#icon-959-62AF44',
+        '#icon-959-62AF44-labelson-nodesc'
+    ],
+    'icon-959-DB4436.png': [
+        '#icon-959-DB4436',
+        '#icon-959-DB4436-labelson',
+        '#icon-959-DB4436-labelson-nodesc',
+        '#icon-959-DB4436-nodesc'
+    ],
+    'icon-960-009D57.png': [
+        '#icon-960-009D57',
+        '#icon-960-009D57-labelson',
+        '#icon-960-009D57-labelson-nodesc',
+        '#icon-960-009D57-nodesc'
+    ],
+    'icon-960-62AF44.png': [
+        '#icon-960-62AF44',
+        '#icon-960-62AF44-nodesc'
+    ],
+    'icon-960-DB4436.png': [
+        '#icon-960-DB4436',
+        '#icon-960-DB4436-labelson'
+    ],
+    'icon-961-009D57.png': [
+        '#icon-961-009D57-labelson'
+    ],
+    'icon-961-4186F0.png': [
+        '#icon-961-4186F0',
+        '#icon-961-4186F0-nodesc'
+    ],
+    'icon-962-009D57.png': [
+        '#icon-962-009D57-labelson',
+        '#icon-962-009D57-labelson-nodesc'
+    ],
+    'icon-962-7C3592.png': [
+        '#icon-962-7C3592-labelson'
+    ],
+    'icon-962-B29189.png': [
+        '#icon-962-B29189-labelson',
+        '#icon-962-B29189-labelson-nodesc'
+    ],
+    'icon-962-DB4436.png': [
+        '#icon-962-DB4436-labelson'
+    ],
+    'icon-962-F8971B.png': [
+        '#icon-962-F8971B-labelson',
+        '#icon-962-F8971B-labelson-nodesc'
+    ]
+}
+
+
+def get_icon(icons, id):
+    for key, value in icons.items():
+        if id in value:
+            return key
+
+print(get_icon(icons, '#icon-962-F8971B-labelson'))
--- app/tests.py
+++ app/tests.py
@@ -1,41 +1,20 @@
-import re
-
-from get_pictograms import soup
-
-
-# def get_placemark(soup):
-#     placemark_set = set()
-#     all_placemarks = soup.find_all('Placemark')
-#     for placemark in all_placemarks:
-#         placemark_set.add(placemark.find('styleUrl').text)
-#     return list(placemark_set)
+# icons ={
+#     'icon-1001.png': ['#icon-1001', '#icon-1001-labelson', '#icon-1001-labelson-nodesc', '#icon-1001-nodesc'],
+#     'icon-1002.png': ['#icon-1002', '#icon-1001-labelson', '#icon-1001-labelson-nodesc', '#icon-1001-nodesc'],
+# }
+#
+# def get_key(d, value):
+#     for key, val in d.items():
+#         if value in val:
+#             return key
 #
 #
-# placemarks = get_placemark(soup)
-#
-# icons = []
-# lines = []
-# for i in placemarks:
-#     if re.search('^#line-', i) is not None:
-#         lines.append(i)
-#
-# sort_icons = sorted(lines)
-#
-# for i in sort_icons:
-#     print(i)
-#
-# print(get_placemark(soup))
+# print(get_key(icons, '#icon-1001'))
 
+with open('i_sort.txt', encoding='utf-8') as n:
+    name = n.read().split('\n')
 
-icons ={
-    'icon-1001.png': ['#icon-1001', '#icon-1001-labelson', '#icon-1001-labelson-nodesc', '#icon-1001-nodesc'],
-    'icon-1002.png': ['#icon-1002', '#icon-1001-labelson', '#icon-1001-labelson-nodesc', '#icon-1001-nodesc'],
-}
+# for i in name:
+#     dictionary.setdefault(i, [])
 
-def get_key(d, value):
-    for key, val in d.items():
-        if value in val:
-            return key
-
-
-print(get_key(icons, '#icon-1001'))
+print(name)
